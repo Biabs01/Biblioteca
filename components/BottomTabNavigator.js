dd09c2d42e6c2ce10d42a4dc8d0570c72f0be87c
Biabs01 <108082691+Biabs01@users.noreply.github.com>
2023-11-14 18:48:53 -0300

    modified:   App.js new file:   components/BottomTabNavigator.js new file:   package-lock.json modified:   package.json new file:   screens/Search.js new file:   screens/Transactions.js modified:   yarn.lock

---
+++ components/BottomTabNavigator.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { NavigationContainer } from '@react-navigation/native';;
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import TransactionScreen from '../screens/Transactions';
+import SearchScreen from '../screens/Search';
+
+const Tab = createBottomTabNavigator();
+
+export default class BottomTabNavigator extends Component{
+    render(){
+        return(
+            <NavigationContainer>
+                <Tab.Navigator>
+                    <Tab.Screen name='Transação' component={TransactionScreen}/>
+                    <Tab.Screen name='Pesquisa' component={SearchScreen}/>
+                </Tab.Navigator>
+            </NavigationContainer>
+        )
+    }  
+}
