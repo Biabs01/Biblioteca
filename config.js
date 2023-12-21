e64bc6b4f310b39a5a25c542878c739857f1f8b0
Biabs01 <108082691+Biabs01@users.noreply.github.com>
2023-12-21 20:06:28 -0300

    new file:   config.js 	modified:   package-lock.json 	modified:   package.json 	modified:   yarn.lock

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBD0DSDkA1mO2LpMzcMoPlPokds0f7DKI8",
+    authDomain: "bibliotecatq7.firebaseapp.com",
+    projectId: "bibliotecatq7",
+    storageBucket: "bibliotecatq7.appspot.com",
+    messagingSenderId: "907276384869",
+    appId: "1:907276384869:web:42d612e4a73ddcd9b7a48c"
+  };
